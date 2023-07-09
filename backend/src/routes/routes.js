4163e0df3d8bfd9aa74d68b12bc7b586d6517e86
Shubhamraut9497 <114891419+Shubhamraut9497@users.noreply.github.com>
2023-07-09 10:18:44 +0530

    initial commit

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -14,10 +14,21 @@ import cookieParser from "cookie-parser";
 import multer from "multer";
 
 const uploadMiddleware = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Specify the directory where you want to save the uploaded files
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    // Set the name of the uploaded file within the destination directory
+
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
 
 const router = express.Router();
 
-router.use(cookieParser()); 
+router.use(cookieParser());
 
 router.post("/register", registerUser);
 router.post("/login", LoginUser);
@@ -27,6 +38,6 @@ router.post("/createNewUser", uploadMiddleware.single("file"), createNewUser);
 router.get("/createNewUser", getCustomerData);
 router.get("/createNewUser/:id", getSingleCustomerData);
 router.put("/createNewUser", uploadMiddleware.single("file"), UpdateCustomer);
-router.delete("/createNewUser/:id",deleteCustomer)
+router.delete("/createNewUser/:id", deleteCustomer);
 
 export default router;
